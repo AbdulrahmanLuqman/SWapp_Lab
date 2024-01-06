ea71f1294f6fd49ef3f340a2c269359d5d38acf8
AbdulrahmanLuqman <fal528@dervac.com>
2024-01-06 01:08:02 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script>
+import { defineComponent } from 'vue';
+import SideNav from '@/components/SideNav.vue';
+
+export default defineComponent({
+  components:{
+    SideNav
+  },
+  data() {
+    return{
+
+    }
+  }
+})
+</script>
+
+<template>
+  <main>
+    <SideNav />
+  </main>
+</template>
